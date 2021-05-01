2cd312ea7234023330d5933b4ae8c2797f8e4061
George Webberley <georgewebberley@googlemail.com>
2021-05-01 14:08:27 +0200

    first commit:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { LandingComponent } from './landing/landing.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: LandingComponent },
+  { path: 'host', loadChildren: () => import('./host/host.module').then(m => m.HostModule) },
+  { path: 'players', loadChildren: () => import('./players/players.module').then(m => m.PlayersModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
